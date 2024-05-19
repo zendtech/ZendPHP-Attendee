877eab19944b06121f13006488c457850e99554c
dbierer <doug@unlikelysource.com>
2024-05-19 08:05:26 +0700

    2024-05-19 DB: Updating VM infrastructure

--- VM/install_extra.sh
+++ VM/install_extra.sh
@@ -6,6 +6,4 @@ chown -R vagrant:www-data $HOME_DIR/*
 find $HOME_DIR/* -type f -exec chmod 664 {} \;
 find $HOME_DIR/* -type d -exec chmod 775 {} \;
 chown -R www-data:www-data /var/www
-echo "Misc cleanup ... "
-apt-get -y autoremove
 echo "Course-specific VM setup complete ... "
