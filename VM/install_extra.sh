239d6eeee337a3bdb608861b3233723599663093
unknown <doug@unlikelysource.com>
2024-12-12 21:56:44 +0700

    2024-12-12: Updated VM installation + set up blank database file for demo app

--- VM/install_extra.sh
+++ VM/install_extra.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/sh
 . ../Docker/secrets.sh
-echo "Installing misc tools ..." && \
+echo "Installing misc tools ..."
 apt-get update -y
-apt-get install -y podman podman-compose
+apt-get install -y podman podman-compose sqlite3
 ln -s /usr/bin/podman /usr/bin/docker
 ln -s /usr/bin/podman-compose /usr/bin/docker-compose
+echo "Setting up demo app ..."
+cp $REPO_DIR/Zend/Course_Assets/training.db $REPO_DIR/Zend/Basic_Installation/mezzio/data/training.db
+cd $REPO_DIR/Zend/Basic_Installation/mezzio/
+composer install
 echo "Assigning permissions ..."
 chmod 755 $HOME_DIR
 chown -R vagrant:www-data $HOME_DIR/*
